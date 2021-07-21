3b3a8b3fd4bdfde1aea592a8509734131aa3c4a0
DoanTrongThang <email_cua_ban>
2021-07-21 16:28:16 +0700

    Homework by NDT

---
+++ database/seeders/ApartmentsSeeder.php
@@ -0,0 +1,186 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class ApartmentsSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $tableName = 'apartment';
+        DB::statement('SET FOREIGN_KEY_CHECKS = 0');
+        DB::table($tableName)->truncate();
+        DB::table($tableName)->insert([
+            [
+                'name' => 'topazelite',
+                'address' => 'Quận 8',
+                'price' => 22000000000,
+                'generalInformation' & 'detailInformation' => 'Good',
+                'avatar' => 'https://cdn.chotot.com/9ltUr-Pq4wya4mZWRL-Y6cgjuqns4r8HjVN2jP2i73s/preset:property_project_default/plain/2324_facilities_5.jpg',
+                'status' => 1,
+            ],
+            [
+                'name' => 'topazelite',
+                'address' => 'Quận 8',
+                'price' => 22000000000,
+                'generalInformation' & 'detailInformation' => 'Good',
+                'avatar' => 'https://cdn.chotot.com/9ltUr-Pq4wya4mZWRL-Y6cgjuqns4r8HjVN2jP2i73s/preset:property_project_default/plain/2324_facilities_5.jpg',
+                'status' => 1,
+            ],
+            [
+                'name' => 'topazelite',
+                'address' => 'Quận 8',
+                'price' => 22000000000,
+                'generalInformation' & 'detailInformation' => 'Good',
+                'avatar' => 'https://cdn.chotot.com/9ltUr-Pq4wya4mZWRL-Y6cgjuqns4r8HjVN2jP2i73s/preset:property_project_default/plain/2324_facilities_5.jpg',
+                'status' => 1,
+            ],
+            [
+                'name' => 'topazelite',
+                'address' => 'Quận 8',
+                'price' => 22000000000,
+                'generalInformation' & 'detailInformation' => 'Good',
+                'avatar' => 'https://cdn.chotot.com/9ltUr-Pq4wya4mZWRL-Y6cgjuqns4r8HjVN2jP2i73s/preset:property_project_default/plain/2324_facilities_5.jpg',
+                'status' => 1,
+            ],
+            [
+                'name' => 'topazelite',
+                'address' => 'Quận 8',
+                'price' => 22000000000,
+                'generalInformation' & 'detailInformation' => 'Good',
+                'avatar' => 'https://cdn.chotot.com/9ltUr-Pq4wya4mZWRL-Y6cgjuqns4r8HjVN2jP2i73s/preset:property_project_default/plain/2324_facilities_5.jpg',
+                'status' => 1,
+            ],
+            [
+                'name' => 'topazelite',
+                'address' => 'Quận 8',
+                'price' => 22000000000,
+                'generalInformation' & 'detailInformation' => 'Good',
+                'avatar' => 'https://cdn.chotot.com/9ltUr-Pq4wya4mZWRL-Y6cgjuqns4r8HjVN2jP2i73s/preset:property_project_default/plain/2324_facilities_5.jpg',
+                'status' => 1,
+            ],
+            [
+                'name' => 'topazelite',
+                'address' => 'Quận 8',
+                'price' => 22000000000,
+                'generalInformation' & 'detailInformation' => 'Good',
+                'avatar' => 'https://cdn.chotot.com/9ltUr-Pq4wya4mZWRL-Y6cgjuqns4r8HjVN2jP2i73s/preset:property_project_default/plain/2324_facilities_5.jpg',
+                'status' => 1,
+            ],
+            [
+                'name' => 'topazelite',
+                'address' => 'Quận 8',
+                'price' => 22000000000,
+                'generalInformation' & 'detailInformation' => 'Good',
+                'avatar' => 'https://cdn.chotot.com/9ltUr-Pq4wya4mZWRL-Y6cgjuqns4r8HjVN2jP2i73s/preset:property_project_default/plain/2324_facilities_5.jpg',
+                'status' => 1,
+            ],
+            [
+                'name' => 'topazelite',
+                'address' => 'Quận 8',
+                'price' => 22000000000,
+                'generalInformation' & 'detailInformation' => 'Good',
+                'avatar' => 'https://cdn.chotot.com/9ltUr-Pq4wya4mZWRL-Y6cgjuqns4r8HjVN2jP2i73s/preset:property_project_default/plain/2324_facilities_5.jpg',
+                'status' => 1,
+            ],
+            [
+                'name' => 'topazelite',
+                'address' => 'Quận 8',
+                'price' => 22000000000,
+                'generalInformation' & 'detailInformation' => 'Good',
+                'avatar' => 'https://cdn.chotot.com/9ltUr-Pq4wya4mZWRL-Y6cgjuqns4r8HjVN2jP2i73s/preset:property_project_default/plain/2324_facilities_5.jpg',
+                'status' => 1,
+            ],[
+                'name' => 'topazelite',
+                'address' => 'Quận 8',
+                'price' => 22000000000,
+                'generalInformation' & 'detailInformation' => 'Good',
+                'avatar' => 'https://cdn.chotot.com/9ltUr-Pq4wya4mZWRL-Y6cgjuqns4r8HjVN2jP2i73s/preset:property_project_default/plain/2324_facilities_5.jpg',
+                'status' => 1,
+            ],
+            [
+                'name' => 'topazelite',
+                'address' => 'Quận 8',
+                'price' => 22000000000,
+                'generalInformation' & 'detailInformation' => 'Good',
+                'avatar' => 'https://cdn.chotot.com/9ltUr-Pq4wya4mZWRL-Y6cgjuqns4r8HjVN2jP2i73s/preset:property_project_default/plain/2324_facilities_5.jpg',
+                'status' => 1,
+            ],
+            [
+                'name' => 'topazelite',
+                'address' => 'Quận 8',
+                'price' => 22000000000,
+                'generalInformation' & 'detailInformation' => 'Good',
+                'avatar' => 'https://cdn.chotot.com/9ltUr-Pq4wya4mZWRL-Y6cgjuqns4r8HjVN2jP2i73s/preset:property_project_default/plain/2324_facilities_5.jpg',
+                'status' => 1,
+            ],
+            [
+                'name' => 'topazelite',
+                'address' => 'Quận 8',
+                'price' => 22000000000,
+                'generalInformation' & 'detailInformation' => 'Good',
+                'avatar' => 'https://cdn.chotot.com/9ltUr-Pq4wya4mZWRL-Y6cgjuqns4r8HjVN2jP2i73s/preset:property_project_default/plain/2324_facilities_5.jpg',
+                'status' => 1,
+            ],
+            [
+                'name' => 'topazelite',
+                'address' => 'Quận 8',
+                'price' => 22000000000,
+                'generalInformation' & 'detailInformation' => 'Good',
+                'avatar' => 'https://cdn.chotot.com/9ltUr-Pq4wya4mZWRL-Y6cgjuqns4r8HjVN2jP2i73s/preset:property_project_default/plain/2324_facilities_5.jpg',
+                'status' => 1,
+            ],
+            [
+                'name' => 'topazelite',
+                'address' => 'Quận 8',
+                'price' => 22000000000,
+                'generalInformation' & 'detailInformation' => 'Good',
+                'avatar' => 'https://cdn.chotot.com/9ltUr-Pq4wya4mZWRL-Y6cgjuqns4r8HjVN2jP2i73s/preset:property_project_default/plain/2324_facilities_5.jpg',
+                'status' => 1,
+            ],
+            [
+                'name' => 'topazelite',
+                'address' => 'Quận 8',
+                'price' => 22000000000,
+                'generalInformation' & 'detailInformation' => 'Good',
+                'avatar' => 'https://cdn.chotot.com/9ltUr-Pq4wya4mZWRL-Y6cgjuqns4r8HjVN2jP2i73s/preset:property_project_default/plain/2324_facilities_5.jpg',
+                'status' => 1,
+            ],
+            [
+                'name' => 'topazelite',
+                'address' => 'Quận 8',
+                'price' => 22000000000,
+                'generalInformation' & 'detailInformation' => 'Good',
+                'avatar' => 'https://cdn.chotot.com/9ltUr-Pq4wya4mZWRL-Y6cgjuqns4r8HjVN2jP2i73s/preset:property_project_default/plain/2324_facilities_5.jpg',
+                'status' => 1,
+            ],
+            [
+                'name' => 'topazelite',
+                'address' => 'Quận 8',
+                'price' => 22000000000,
+                'generalInformation' & 'detailInformation' => 'Good',
+                'avatar' => 'https://cdn.chotot.com/9ltUr-Pq4wya4mZWRL-Y6cgjuqns4r8HjVN2jP2i73s/preset:property_project_default/plain/2324_facilities_5.jpg',
+                'status' => 1,
+            ],
+            [
+                'name' => 'topazelite',
+                'address' => 'Quận 8',
+                'price' => 22000000000,
+                'generalInformation' & 'detailInformation' => 'Good',
+                'avatar' => 'https://cdn.chotot.com/9ltUr-Pq4wya4mZWRL-Y6cgjuqns4r8HjVN2jP2i73s/preset:property_project_default/plain/2324_facilities_5.jpg',
+                'status' => 1,
+            ],
+
+
+        ]);
+        DB::statement('SET FOREIGN_KEY_CHECKS = 1');
+
+
+    }
+}
